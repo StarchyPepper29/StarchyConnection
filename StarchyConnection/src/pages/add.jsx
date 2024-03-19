ce22fec23d4bb369dd7fa9dc1fc79c5552803c6a
Umais Usmani <98653230+StarchyPepper29@users.noreply.github.com>
2024-03-19 17:53:00 +0500

    Adding a Post implemented

--- StarchyConnection/src/pages/add.jsx
+++ StarchyConnection/src/pages/add.jsx
@@ -1,11 +1,71 @@
 import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 const add = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [author, setAuthor] = useState('');
+
+
+  const handleSubmit = () => {
+    const newPost = {
+      title: title,
+      author: author,
+      tags : ["new"],
+      body: description,
+     
+    };
+    axios.post('http://localhost:3000/blogs', newPost)
+    .then((response) => {
+      console.log(response);
+    }).then(() => {
+      navigate('/blog');
+    }
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
   return (
     <div>
-        <h1>Add</h1>
-    </div>
-  )
-}
+    <h1>Add a New Post</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="author">Author:</label>
+        <input
+          type="text"
+          id="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+      </div>
+      <button type="submit">Add Post</button>
+    </form>
+  </div>
+  );
+};
+  
 
 export default add
--- StarchyConnection/src/pages/home.jsx
+++ StarchyConnection/src/pages/home.jsx
@@ -6,6 +6,13 @@ const home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <p>Welcome to Starchy Connection</p>
+      <Link to={`add`}>
+      <button>Add your own blog post</button>
+      </Link>
+      <Link to={`blog`}>
+        <button>View all blog posts</button>
+      </Link>
     </div>
   )
 }
